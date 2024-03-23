4151a9048a2227ec6adfb4d0736eeb5136801fee
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-03-22 21:35:31 -0500

    update

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+composer install
+npm install
+npm run production
+php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate
